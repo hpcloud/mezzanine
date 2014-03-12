d310bd4958d173adab1f8e56eba38f1171a0a9d3
Jan Dubois <jand@activestate.com>
2014-03-12 13:55:21 -0700

    django.conf.urls.defaults has been removed in Django 1.6.
    http://stackoverflow.com/questions/19962736/django-import-error-no-module-named-django-conf-urls-defaults

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
